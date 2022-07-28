b2ac9d36eaa5d60a1e8bea6ff1e9aafb161582a8
Armando <hello.armandocrn@gmail.com>
2022-07-28 13:06:58 +0200

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+Praticamente qui lasciamo solo la funzione che sarà presente in webpage.js 
+così che svolga il suo ruolo
+
+ma per iniziare dovrò scrivere prima cosa succede(?)
+
+vabbe praticamente in webpage.js creiamo la funzione che costruisce l'header
+il footer
+e il main
+in home.js creiamo quello che costruisce tutto ciò all'interno di main
+quando premiamo main
+*/
